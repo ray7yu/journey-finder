01d0da63096c7d9c9980a5a2626f1354e71fed22
Raymond <wrg7yu@gmail.com>
2020-06-09 22:25:04 -0400

    Create python script that uses selenium

---
+++ journeyFinder.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python3
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome('/usr/local/bin/chromedriver') 
+driver.set_window_size(1200, 700)
+driver.implicitly_wait(10)
+driver.get('http://www.tripadvisor.com/') 
+
+#Major issue was that first search bar was hidden and not interactable
+#Search bar
+elements = driver.find_elements_by_name('q')
+# for i in elements:
+#     print(i.get_attribute('aria-label'))
+search = elements[1]
+search.click()
+search.send_keys('Tallahassee')
+search.submit()
+
+'''Overview of City'''
+results = driver.find_elements_by_class_name('result-title')
+city = results[0]
+city.click()
+
+'''Go to Things To Do'''
+allWindows = driver.window_handles
+parentWindow = allWindows[0]
+currWindow = allWindows[1]
+driver.switch_to.window(currWindow)
+
+'''Go to attractions'''
+attractions = driver.find_elements_by_class_name('_1yB-kafB')
+if not attractions:
+    print('empty')
+attractions[1].click()
+
+'''Scrolls and makes more locations available'''
+time.sleep(3)
+see_more = driver.find_element_by_class_name('attractions-attraction-overview-main-TopPOIs__see_more--2Vsb-')
+#scrolls to element
+driver.execute_script("arguments[0].scrollIntoView();", see_more)
+#scrolls a little up
+driver.execute_script("window.scrollBy(0, -80);")
+time.sleep(3)
+see_more.click()
+
+'''Collect attractions'''
+names = []
+reviews = []
+urls = []
+anchors = driver.find_elements_by_class_name('attractions-attraction-overview-pois-PoiInfo__name--SJ0a4')
+for a in anchors:
+    urls.append(a.get_attribute('href'))
+    place = a.find_element_by_tag_name('h3')
+    names.append(place.text)
+numbers = driver.find_elements_by_class_name('reviewCount')
+for n in numbers:
+    reviews.append(n.text)
+print(urls)
+print(names)
+print(reviews)
+
+'''End Scraper'''
+time.sleep(3) # Let the user actually see something!
+driver.quit()
